55cd75bbb31d239179d55057cd77f5e878a8acf3
Riccardo Schirone <sirmy15@gmail.com>
2018-07-23 12:11:09 +0200

    add an initial test for io prioritize issue

--- unit/test_io.c
+++ unit/test_io.c
@@ -26,14 +26,13 @@ bool test_r_io_pcache (void) {
 	RIO *io = r_io_new ();
 	ut8 buf[8];
 	int fd = r_io_fd_open (io, "malloc://3", R_IO_RW, 0);
-	r_io_map_add (io, fd, R_IO_RW, 0LL, 0LL, 1, false); //8
-	r_io_map_add (io, fd, R_IO_RW, 1, 1, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 2, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 3, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 4, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 5, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 2, 6, 1, false); //D
-	r_io_map_calculate_skyline (io);
+	r_io_map_add (io, fd, R_IO_RW, 0LL, 0LL, 1); //8
+	r_io_map_add (io, fd, R_IO_RW, 1, 1, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 2, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 3, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 4, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 5, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 2, 6, 1); //D
 	io->p_cache = 2;
 	io->va = true;
 	r_io_fd_write_at (io, fd, 0, (const ut8*)"8=D", 3);
@@ -62,11 +61,54 @@ bool test_r_io_desc_exchange (void) {
 	mu_end;
 }
 
+bool test_r_io_priority(void) {
+	RIO *io = r_io_new();
+	ut32 map0, map1;
+	ut64 buf;
+	bool ret;
+
+	io->va = true;
+	r_io_open_at (io, "malloc://8", R_IO_RW, 0644, 0x0);
+	map0 = r_io_map_get (io, 0)->id;
+	ret = r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert ("should be able to read", ret); 
+	mu_assert_memeq ((ut8*)&buf, "\x00\x00\x00\x00\x00\x00\x00\x00", 8, "0 should be there initially");
+	buf = 0x9090909090909090;
+	r_io_write_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x90\x90\x90\x90\x90\x90\x90\x90", 8, "0x90 should have been written");
+
+	r_io_open_at (io, "malloc://2", R_IO_RW, 0644, 0x4);
+	map1 = r_io_map_get (io, 4)->id;
+	r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x90\x90\x90\x90\x00\x00\x90\x90", 8, "0x00 from map1 should overlap");
+
+	buf ^= UT64_MAX;
+	r_io_write_at (io, 0, (ut8 *)&buf, 8);
+	r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x6f\x6f\x6f\x6f\xff\xff\x6f\x6f", 8, "memory has been xored");
+
+	r_io_map_priorize (io, map0);
+	r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x6f\x6f\x6f\x6f\x90\x90\x6f\x6f", 8, "map0 should have been prioritized");
+
+	r_io_map_remap (io, map1, 0x2);
+	r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x6f\x6f\x6f\x6f\x90\x90\x6f\x6f", 8, "map1 should have been remapped");
+
+	r_io_map_priorize (io, map1);
+	r_io_read_at (io, 0, (ut8 *)&buf, 8);
+	mu_assert_memeq ((ut8*)&buf, "\x6f\x6f\xff\xff\x90\x90\x6f\x6f", 8, "map1 should have been prioritized");
+
+	r_io_free (io);
+	mu_end;
+}
+
 int all_tests() {
 	mu_run_test(test_r_io_mapsplit);
 	mu_run_test(test_r_io_mapsplit2);
 	mu_run_test(test_r_io_pcache);
 	mu_run_test(test_r_io_desc_exchange);
+	mu_run_test(test_r_io_priority);
 	return tests_passed != tests_run;
 }
 
