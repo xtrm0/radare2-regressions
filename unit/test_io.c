c363b5882a3a55b1a4b2beb56c1f3b4ad972f54f
Riccardo Schirone <ret2libc@users.noreply.github.com>
2018-09-03 11:18:44 +0200

    fixes for updates to r_io API (#1473)

--- unit/test_io.c
+++ unit/test_io.c
@@ -26,14 +26,13 @@ bool test_r_io_pcache (void) {
 	RIO *io = r_io_new ();
 	ut8 buf[8];
 	int fd = r_io_fd_open (io, "malloc://3", R_IO_RW, 0);
-	r_io_map_add (io, fd, R_IO_RW, 0LL, 0LL, 1, false); //8
-	r_io_map_add (io, fd, R_IO_RW, 1, 1, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 2, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 3, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 4, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 5, 1, false); //=
-	r_io_map_add (io, fd, R_IO_RW, 2, 6, 1, false); //D
-	r_io_map_calculate_skyline (io);
+	r_io_map_add (io, fd, R_IO_RW, 0LL, 0LL, 1); //8
+	r_io_map_add (io, fd, R_IO_RW, 1, 1, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 2, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 3, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 4, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 1, 5, 1); //=
+	r_io_map_add (io, fd, R_IO_RW, 2, 6, 1); //D
 	io->p_cache = 2;
 	io->va = true;
 	r_io_fd_write_at (io, fd, 0, (const ut8*)"8=D", 3);
@@ -78,6 +77,7 @@ bool test_va_malloc_zero(void) {
 
 	io = r_io_new ();
 	io->va = true;
+	r_io_open_at (io, "malloc://8", R_IO_RW, 0644, 0x0);
 	buf = 0xdeadbeefcafebabe;
 	ret = r_io_read_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert ("should be able to read", ret);
@@ -135,7 +135,7 @@ int all_tests() {
 	mu_run_test(test_r_io_mapsplit2);
 	mu_run_test(test_r_io_pcache);
 	mu_run_test(test_r_io_desc_exchange);
-	// mu_run_test(test_r_io_priority);
+	mu_run_test(test_r_io_priority);
 	mu_run_test(test_va_malloc_zero);
 	return tests_passed != tests_run;
 }
