c7baf4cc21a3951b72dcd29f154f602c00f192ca
pancake <pancake@nopcode.org>
2018-09-22 12:59:59 +0200

    Fix unit tests

--- unit/test_io.c
+++ unit/test_io.c
@@ -4,7 +4,7 @@
 bool test_r_io_mapsplit (void) {
 	RIO *io = r_io_new ();
 	io->va = true;
-	r_io_open_at (io, "null://2", R_IO_READ, 0LL, UT64_MAX);
+	r_io_open_at (io, "null://2", R_PERM_R, 0LL, UT64_MAX);
 	mu_assert ("Found no map at UT64", r_io_map_get (io, UT64_MAX));
 	mu_assert ("Found no map at 0x0", r_io_map_get (io, 0x0));
 	r_io_free (io);
@@ -14,7 +14,7 @@ bool test_r_io_mapsplit (void) {
 bool test_r_io_mapsplit2 (void) {
 	RIO *io = r_io_new ();
 	io->va = true;
-	r_io_open_at (io, "null://2", R_IO_READ, 0LL, 0LL);
+	r_io_open_at (io, "null://2", R_PERM_R, 0LL, 0LL);
 	r_io_map_remap (io, r_io_map_get (io, 0LL)->id, UT64_MAX);
 	mu_assert ("Found no map at UT64", r_io_map_get (io, UT64_MAX));
 	mu_assert ("Found no map at 0x0", r_io_map_get (io, 0x0));
@@ -25,14 +25,14 @@ bool test_r_io_mapsplit2 (void) {
 bool test_r_io_pcache (void) {
 	RIO *io = r_io_new ();
 	ut8 buf[8];
-	int fd = r_io_fd_open (io, "malloc://3", R_IO_RW, 0);
-	r_io_map_add (io, fd, R_IO_RW, 0LL, 0LL, 1); //8
-	r_io_map_add (io, fd, R_IO_RW, 1, 1, 1); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 2, 1); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 3, 1); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 4, 1); //=
-	r_io_map_add (io, fd, R_IO_RW, 1, 5, 1); //=
-	r_io_map_add (io, fd, R_IO_RW, 2, 6, 1); //D
+	int fd = r_io_fd_open (io, "malloc://3", R_PERM_RW, 0);
+	r_io_map_add (io, fd, R_PERM_RW, 0LL, 0LL, 1); //8
+	r_io_map_add (io, fd, R_PERM_RW, 1, 1, 1); //=
+	r_io_map_add (io, fd, R_PERM_RW, 1, 2, 1); //=
+	r_io_map_add (io, fd, R_PERM_RW, 1, 3, 1); //=
+	r_io_map_add (io, fd, R_PERM_RW, 1, 4, 1); //=
+	r_io_map_add (io, fd, R_PERM_RW, 1, 5, 1); //=
+	r_io_map_add (io, fd, R_PERM_RW, 2, 6, 1); //D
 	io->p_cache = 2;
 	io->va = true;
 	r_io_fd_write_at (io, fd, 0, (const ut8*)"8=D", 3);
@@ -53,8 +53,8 @@ bool test_r_io_pcache (void) {
 
 bool test_r_io_desc_exchange (void) {
 	RIO *io = r_io_new ();
-	int fd = r_io_fd_open (io, "malloc://3", R_IO_READ, 0),
-	    fdx = r_io_fd_open (io, "malloc://6", R_IO_READ, 0);
+	int fd = r_io_fd_open (io, "malloc://3", R_PERM_R, 0),
+	    fdx = r_io_fd_open (io, "malloc://6", R_PERM_R, 0);
 	r_io_desc_exchange (io, fd, fdx);
 	mu_assert ("Desc-exchange is broken", (r_io_fd_size (io, fd) == 6));
 	r_io_free (io);
@@ -68,7 +68,7 @@ bool test_va_malloc_zero(void) {
 
 	io = r_io_new ();
 	io->va = false;
-	r_io_open_at (io, "malloc://8", R_IO_RW, 0644, 0x0);
+	r_io_open_at (io, "malloc://8", R_PERM_RW, 0644, 0x0);
 	buf = 0xdeadbeefcafebabe;
 	ret = r_io_read_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert ("should be able to read", ret);
@@ -77,7 +77,7 @@ bool test_va_malloc_zero(void) {
 
 	io = r_io_new ();
 	io->va = true;
-	r_io_open_at (io, "malloc://8", R_IO_RW, 0644, 0x0);
+	r_io_open_at (io, "malloc://8", R_PERM_RW, 0644, 0x0);
 	buf = 0xdeadbeefcafebabe;
 	ret = r_io_read_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert ("should be able to read", ret);
@@ -95,7 +95,7 @@ bool test_r_io_priority(void) {
 	bool ret;
 
 	io->va = true;
-	r_io_open_at (io, "malloc://8", R_IO_RW, 0644, 0x0);
+	r_io_open_at (io, "malloc://8", R_PERM_RW, 0644, 0x0);
 	map0 = r_io_map_get (io, 0)->id;
 	ret = r_io_read_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert ("should be able to read", ret);
@@ -104,7 +104,7 @@ bool test_r_io_priority(void) {
 	r_io_write_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert_memeq ((ut8 *)&buf, (ut8 *)"\x90\x90\x90\x90\x90\x90\x90\x90", 8, "0x90 should have been written");
 
-	r_io_open_at (io, "malloc://2", R_IO_RW, 0644, 0x4);
+	r_io_open_at (io, "malloc://2", R_PERM_RW, 0644, 0x4);
 	map1 = r_io_map_get (io, 4)->id;
 	r_io_read_at (io, 0, (ut8 *)&buf, 8);
 	mu_assert_memeq ((ut8 *)&buf, (ut8 *)"\x90\x90\x90\x90\x00\x00\x90\x90", 8, "0x00 from map1 should overlap");
